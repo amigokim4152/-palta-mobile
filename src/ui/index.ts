f742c89609e8aa7e84ee911658691e3dcb63b682
amigokim4152 <kimeuisin@gmail.com>
2026-09-17 13:50:13 -0300

    design-system: export Living Example registry

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,6 +2,7 @@ export * from "./tokens.js";
 export * from "./icons.js";
 export * from "./characters.js";
 export * from "./reactions.js";
+export * from "./livingExamples.js";
 export * from "./localeQa.js";
 export * from "./accessibilityQa.js";
 export * from "./qaPersonas.js";
