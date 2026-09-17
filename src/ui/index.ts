2b536b9f2241651143368ab40e176ad948328057
amigokim4152 <kimeuisin@gmail.com>
2026-09-17 13:45:56 -0300

    design-system: export character reaction and QA contracts

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,7 @@
 export * from "./tokens.js";
 export * from "./icons.js";
+export * from "./characters.js";
+export * from "./reactions.js";
+export * from "./localeQa.js";
+export * from "./accessibilityQa.js";
 export * from "./contracts.js";
