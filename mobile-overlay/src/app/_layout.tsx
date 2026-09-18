810b3fb9ebfd1dd9fcf66b4e37b67d852727c50c
amigokim4152 <kimeuisin@gmail.com>
2026-09-18 07:39:23 -0300

    feat: mount localization provider in mobile runtime

--- mobile-overlay/src/app/_layout.tsx
+++ mobile-overlay/src/app/_layout.tsx
@@ -3,19 +3,22 @@ import { MutationSyncBootstrap } from '../providers/MutationSyncBootstrap';
 import { PaltaSQLiteProvider } from '../providers/PaltaSQLiteProvider';
 import { NeighborhoodStateProvider } from '../state/NeighborhoodStateProvider';
 import { AuthRuntimeProvider } from '../providers/AuthRuntimeProvider';
+import { LocalizationProvider } from '../providers/LocalizationProvider';
 import { AuthGate } from '../features/auth/AuthGate';
 
 export default function RootLayout() {
   return (
     <AuthRuntimeProvider>
-      <AuthGate>
-        <PaltaSQLiteProvider>
-          <MutationSyncBootstrap />
-          <NeighborhoodStateProvider>
-            <Stack screenOptions={{ headerShown: false }} />
-          </NeighborhoodStateProvider>
-        </PaltaSQLiteProvider>
-      </AuthGate>
+      <LocalizationProvider>
+        <AuthGate>
+          <PaltaSQLiteProvider>
+            <MutationSyncBootstrap />
+            <NeighborhoodStateProvider>
+              <Stack screenOptions={{ headerShown: false }} />
+            </NeighborhoodStateProvider>
+          </PaltaSQLiteProvider>
+        </AuthGate>
+      </LocalizationProvider>
     </AuthRuntimeProvider>
   );
 }
