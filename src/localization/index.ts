53bea11349f7b829be2bc0a633e879e31dd20b53
amigokim4152 <kimeuisin@gmail.com>
2026-09-18 07:29:27 -0300

    feat: export localization core public API

---
+++ src/localization/index.ts
@@ -0,0 +1,31 @@
+export {
+  DEFAULT_CURRENCY,
+  DEFAULT_LANGUAGE_CONTEXT,
+  DEFAULT_LOCALE,
+  DEFAULT_REGION,
+  DEFAULT_TIMEZONE,
+  SUPPORTED_LOCALES,
+  createLanguageContext,
+  isSupportedLocale,
+  resolveInitialLocale,
+  tryNormalizeLocale,
+  type LanguageContext,
+  type PaltaLocale,
+} from './locales.js';
+
+export {
+  ES_CL_UI,
+  resolveUiText,
+  t,
+  type UiKey,
+  type UiTranslationResult,
+} from './uiCatalog.js';
+
+export {
+  hasTranslation,
+  resolveLocalizedContent,
+  type ContentTranslation,
+  type LocalizedContent,
+  type ResolvedContent,
+  type TranslationStatus,
+} from './contentResolver.js';
