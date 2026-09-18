e3073da1b608321598c5a53ffab2978e0b3f3497
amigokim4152 <kimeuisin@gmail.com>
2026-09-18 05:19:24 -0300

    Polish Palta map markers and cluster hierarchy

--- mobile-overlay/src/components/map/NeighborhoodMap.tsx
+++ mobile-overlay/src/components/map/NeighborhoodMap.tsx
@@ -6,6 +6,7 @@ import {
 } from '@maplibre/maplibre-react-native';
 import type { MapFeature } from '../../../../src/adapters/mapCore';
 import { toPointFeatureCollection } from '../../../../src/map/mapFeatureCollection';
+import { paltaTheme } from '../../theme/paltaTheme';
 
 type Props = {
   mapStyle: string;
@@ -57,7 +58,7 @@ export function NeighborhoodMap({
         id="palta-local-entities"
         data={data}
         cluster
-        clusterRadius={42}
+        clusterRadius={46}
         onPress={(event) => {
           const feature = event.nativeEvent.features?.[0];
           const entityId = feature?.properties?.entityId;
@@ -67,14 +68,78 @@ export function NeighborhoodMap({
         }}
       >
         <Layer
+          id="palta-local-point-halo"
+          type="circle"
+          filter={['!', ['has', 'point_count']]}
+          paint={{
+            'circle-color': '#FFFFFF',
+            'circle-radius': [
+              'case',
+              ['==', ['get', 'selected'], true],
+              12,
+              10,
+            ],
+            'circle-opacity': 0.96,
+          }}
+        />
+        <Layer
           id="palta-local-points"
           type="circle"
           filter={['!', ['has', 'point_count']]}
+          paint={{
+            'circle-color': [
+              'case',
+              ['==', ['get', 'selected'], true],
+              paltaTheme.color.brandFresh,
+              paltaTheme.color.brandPrimary,
+            ],
+            'circle-radius': [
+              'case',
+              ['==', ['get', 'selected'], true],
+              8,
+              6.5,
+            ],
+            'circle-stroke-color': '#FFFFFF',
+            'circle-stroke-width': 1.5,
+          }}
+        />
+        <Layer
+          id="palta-local-cluster-halo"
+          type="circle"
+          filter={['has', 'point_count']}
+          paint={{
+            'circle-color': '#FFFFFF',
+            'circle-radius': [
+              'step',
+              ['get', 'point_count'],
+              16,
+              10,
+              20,
+              50,
+              24,
+            ],
+            'circle-opacity': 0.96,
+          }}
         />
         <Layer
           id="palta-local-clusters"
           type="circle"
           filter={['has', 'point_count']}
+          paint={{
+            'circle-color': paltaTheme.color.brandPrimary,
+            'circle-radius': [
+              'step',
+              ['get', 'point_count'],
+              12,
+              10,
+              16,
+              50,
+              20,
+            ],
+            'circle-stroke-color': '#FFFFFF',
+            'circle-stroke-width': 1.5,
+            'circle-opacity': 0.96,
+          }}
         />
       </GeoJSONSource>
     </Map>
