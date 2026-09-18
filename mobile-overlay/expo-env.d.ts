d54d70532942546126f0caefc825d94f0a6afd73
amigokim4152 <kimeuisin@gmail.com>
2026-09-17 22:33:34 -0300

    build(mobile): type Expo runtime environment

---
+++ mobile-overlay/expo-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="expo/types" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    EXPO_PUBLIC_PALTA_API_BASE_URL?: string;
+    EXPO_PUBLIC_MAP_STYLE_URL?: string;
+    EXPO_PUBLIC_ENV?: string;
+  }
+}
