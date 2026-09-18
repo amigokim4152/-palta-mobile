525bf7e805c752478aa6c67ce6380172b1e266bb
amigokim4152 <kimeuisin@gmail.com>
2026-09-18 09:33:18 -0300

    feat(autos): export Chile transaction rules

--- src/autos/index.ts
+++ src/autos/index.ts
@@ -2,3 +2,4 @@ export * from './autosCaptureFlow.js';
 export * from './autosContracts.js';
 export * from './autosEntry.js';
 export * from './autosSellerModel.js';
+export * from './chileVehicleTransaction.js';
