0b3f4c2fe44bf6cc7f808207261c092d3ce2a906
amigokim4152 <kimeuisin@gmail.com>
2026-09-18 08:23:41 -0300

    runtime(auth): gate composed app behind Auth runtime

--- mobile-overlay/src/app/_layout.tsx
+++ mobile-overlay/src/app/_layout.tsx
@@ -1,15 +1,21 @@
 import { Stack } from 'expo-router';
+import { AuthGate } from '../features/auth/AuthGate';
+import { AuthRuntimeProvider } from '../providers/AuthRuntimeProvider';
 import { MutationSyncBootstrap } from '../providers/MutationSyncBootstrap';
 import { PaltaSQLiteProvider } from '../providers/PaltaSQLiteProvider';
 import { NeighborhoodStateProvider } from '../state/NeighborhoodStateProvider';
 
 export default function RootLayout() {
   return (
-    <PaltaSQLiteProvider>
-      <MutationSyncBootstrap />
-      <NeighborhoodStateProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </NeighborhoodStateProvider>
-    </PaltaSQLiteProvider>
+    <AuthRuntimeProvider>
+      <AuthGate>
+        <PaltaSQLiteProvider>
+          <MutationSyncBootstrap />
+          <NeighborhoodStateProvider>
+            <Stack screenOptions={{ headerShown: false }} />
+          </NeighborhoodStateProvider>
+        </PaltaSQLiteProvider>
+      </AuthGate>
+    </AuthRuntimeProvider>
   );
 }
