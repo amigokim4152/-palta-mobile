f59bcce37c5d8dbf0c6803c5e2ad59b4ae05814f
amigokim4152 <kimeuisin@gmail.com>
2026-09-18 08:27:21 -0300

    chore(real-estate): export repository and user state contracts

--- src/realEstate/index.ts
+++ src/realEstate/index.ts
@@ -2,3 +2,5 @@ export * from './realEstateContracts';
 export * from './realEstateDataStatus';
 export * from './realEstateDiscovery';
 export * from './realEstateEntry';
+export * from './realEstateRepository';
+export * from './realEstateUserState';
