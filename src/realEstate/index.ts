fa28433bfdd40db465f1159de7205462fb323531
amigokim4152 <kimeuisin@gmail.com>
2026-09-18 08:34:35 -0300

    chore(real-estate): export publishing contract

--- src/realEstate/index.ts
+++ src/realEstate/index.ts
@@ -2,6 +2,7 @@ export * from './realEstateContracts';
 export * from './realEstateDataStatus';
 export * from './realEstateDiscovery';
 export * from './realEstateEntry';
+export * from './realEstatePublishing';
 export * from './realEstateQueryParams';
 export * from './realEstateRepository';
 export * from './realEstateUserState';
