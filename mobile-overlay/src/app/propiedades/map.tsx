546546e5ae3a4bcd0e667c1c62dc8c3150c2284a
amigokim4152 <kimeuisin@gmail.com>
2026-09-18 08:11:34 -0300

    feat(real-estate): add Propiedades map route

---
+++ mobile-overlay/src/app/propiedades/map.tsx
@@ -0,0 +1,5 @@
+import { PropiedadesScreen } from '../../features/realEstate/PropiedadesScreen';
+
+export default function PropiedadesMapRoute() {
+  return <PropiedadesScreen initialView="map" />;
+}
