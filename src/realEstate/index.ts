0e9a9679c9c68ad040d10029284cb5496a424ec8
amigokim4152 <kimeuisin@gmail.com>
2026-09-18 08:31:23 -0300

    chore(real-estate): export query param contract

--- src/realEstate/index.ts
+++ src/realEstate/index.ts
@@ -2,5 +2,6 @@ export * from './realEstateContracts';
 export * from './realEstateDataStatus';
 export * from './realEstateDiscovery';
 export * from './realEstateEntry';
+export * from './realEstateQueryParams';
 export * from './realEstateRepository';
 export * from './realEstateUserState';
