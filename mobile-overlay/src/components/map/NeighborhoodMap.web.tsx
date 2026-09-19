d9b340ee03a6e2e5358a0c387b0e5b7494e21864
amigokim4152 <kimeuisin@gmail.com>
2026-09-19 00:19:20 -0300

    fix(map): align web cluster labels with Palta font

--- mobile-overlay/src/components/map/NeighborhoodMap.web.tsx
+++ mobile-overlay/src/components/map/NeighborhoodMap.web.tsx
@@ -120,6 +120,7 @@ export function NeighborhoodMap({
         filter: ['has', 'point_count'],
         layout: {
           'text-field': ['get', 'point_count_abbreviated'],
+          'text-font': ['Noto Sans'],
           'text-size': 12,
         },
         paint: {
